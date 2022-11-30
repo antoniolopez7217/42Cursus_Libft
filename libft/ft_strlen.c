f2a5d0d17cee84618de818928dc089a65aaff094
Antonio Lopez <antoniolopez7217@gmail.com>
2022-11-30 23:28:55 +0100

    updated

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -10,4 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+#include <stdio.h>
 
+size_t	ft_strlen(const char *s)
+{
+	int	count;
+	
+	count = 0;
+	while(s[count])
+		count++;
+	return (count);
+}
+
+// int	main(void)
+// {
+// 	char a[20] = "Hello World";
+// 	char b[20] = "";
+// 	char c[20] = "H420.";
+// 	printf("%ld\n", ft_strlen(a));
+// 	printf("%ld\n", ft_strlen(b));
+// 	printf("%ld\n", ft_strlen(c));
+// 	printf("%ld\n", strlen(a));
+// 	printf("%ld\n", strlen(b));
+// 	printf("%ld\n", strlen(c));
+// 	return (0);
+// }
