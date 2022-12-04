9182150b84050473358e6469405ebd18f454409a
Antonio Lopez <antoniolopez7217@gmail.com>
2022-12-04 11:40:00 +0100

    update

---
+++ libft/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antlopez <antlopez@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/04 11:24:13 by antlopez          #+#    #+#             */
+/*   Updated: 2022/12/04 11:24:15 by antlopez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*array;
+
+	array = malloc(nmemb * size); 
+	if (array == NULL)
+		return (0);
+	ft_bzero(array, (nmemb * size));
+	return (array);
+}
