c1e55969201efd296ba66452b0a28c4359c48381
Antonio Lopez <antoniolopez7217@gmail.com>
2022-12-04 20:53:03 +0100

    updated

--- libft/libft.h
+++ libft/libft.h
@@ -44,4 +44,6 @@ int		ft_atoi(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
 #endif
