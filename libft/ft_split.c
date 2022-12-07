1d895adc1867f5a43e4eef45367f7f7f35815808
Antonio Lopez <antoniolopez7217@gmail.com>
2022-12-07 21:31:15 +0100

    updated

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -35,23 +35,43 @@ char	**ft_split(char const *s, char c)
 {
 	char	**str;
 	size_t	i;
-
+	size_t	n;
+	size_t	start;
+	
 	str = malloc(sizeof(char *) * (ft_wordcount(s, c) + 1));
 	if (str == NULL)
 		return (0);
+	n = 0;
+	i = 0;
 	while (s[i])
 	{
-		
-
+		if (s[i] != c)
+		{
+			start = i;
+			while (s[i] && s[i] != c)
+				i++;
+			str[n] = ft_substr(s, start, i - start);
+			n++;
+		}
+		else
+			i++;
 	}
-	
+	str[n] = '\0';
+	return (str);	
 }
 
-int	main(void)
-{
-	char	s[] = "Hello World";
-	char	c = ' ';
+// int	main(void)
+// {
+// 	char	s[] = "Hello World";
+// 	char	c = ' ';
+// 	char	**str = ft_split(s, c);
+// 	int		i;
 
-	printf("%ld\n", ft_wordcount(s, c));
-	return (0);
-}
+// 	i = 0;
+// 	while (str[i])
+// 	{
+// 		printf("%s\n", str[i]);
+// 		i++;
+// 	}
+// 	return (0);
+// }
